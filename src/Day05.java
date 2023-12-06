a5b1e5c4204924f91b55b7f97148b9da665cbcc1
david.kennedy <david.kennedy@workday.com>
2023-12-06 09:32:21 -0800

    day 5 wip

--- src/Day05.java
+++ src/Day05.java
@@ -4,13 +4,17 @@ import static java.util.stream.Collectors.collectingAndThen;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.toSet;
 
 import java.time.Duration;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.EnumMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.regex.Pattern;
 import java.util.stream.IntStream;
@@ -26,10 +30,10 @@ public class Day05 {
     }
 
     public static void main(final String[] args) {
-        final Pair<List<Long>, Almanac> input = parseInput(InputUtil.file("day05"));
+        final Pair<List<Long>, Almanac> input = parseInput(InputUtil.file("test"));
 
         part1(input.right(), input.left());
-        part2(input.right(), input.left());
+        part2_2(input.right(), input.left());
     }
 
     public static void part1(final Almanac almanac, final List<Long> seeds) {
@@ -44,7 +48,7 @@ public class Day05 {
         System.out.println("Part 1: " + solution.right().get(Type.LOCATION));
     }
 
-    public static void part2(final Almanac almanac, final List<Long> seeds) {
+    public static void part2_1(final Almanac almanac, final List<Long> seeds) {
         final List<Range> ranges = IntStream.iterate(0, i -> i < seeds.size() - 1, i -> i + 2)
                 .mapToObj(i -> new Range(seeds.get(i), seeds.get(i) + seeds.get(i + 1)))
                 .toList();
@@ -66,6 +70,26 @@ public class Day05 {
         printSolution(solution.right());
 
         System.out.printf("Part 2 (in %d minutes): %s%n", numMinutes, solution.right().get(Type.LOCATION));
+    }
+
+    public static void part2_2(final Almanac almanac, final List<Long> seeds) {
+        final List<Range> ranges = IntStream.iterate(0, i -> i < seeds.size() - 1, i -> i + 2)
+                .mapToObj(i -> new Range(seeds.get(i), seeds.get(i) + seeds.get(i + 1)))
+                .toList();
+
+        final long start = System.currentTimeMillis();
+
+        final var solution = ranges
+                .stream()
+                .map(seed -> new Pair<>(seed, testSeedRange(almanac, seed)))
+                .mapToLong(p -> p.right().start)
+                .min()
+                .orElseThrow();
+
+        final long end = System.currentTimeMillis();
+        final long numMinutes = Duration.ofMillis(end - start).toMinutes();
+
+        System.out.printf("Part 2 (in %d minutes): %s%n", numMinutes, solution);
     }
 
     // Parse input
@@ -127,6 +151,26 @@ public class Day05 {
         }
     }
 
+    static Range testSeedRange(final Almanac almanac, final Range range) {
+        final var res = testRangeRecur(almanac, Type.SEED, range);
+        return res.stream().sorted().findFirst().orElseThrow();
+    }
+
+    private static Collection<Range> testRangeRecur(final Almanac almanac, final Type curType, final Range range) {
+        if (curType.hasNext()) {
+            final Type next = curType.next();
+            // The parameter range may intersect one-to-many ranges in the entry list, including void ranges
+            return almanac
+                    .almanacMaps
+                    .get(curType)
+                    .bisect(range)
+                    .stream()
+                    .flatMap(r -> testRangeRecur(almanac, next, r).stream())
+                    .collect(toSet());
+        }
+        return Set.of(range);
+    }
+
     // Pretty print
     static void printSolution(final Map<Type, Long> solution) {
         System.out.println(Stream.of(Type.values())
@@ -161,6 +205,32 @@ public class Day05 {
                     .orElse(test);
         }
 
+        Collection<Range> bisect(final Range range) {
+            final Collection<Range> toBeEaten = new HashSet<>();
+            toBeEaten.add(range);
+
+            final Collection<Range> eaten = new HashSet<>();
+
+            for (final Entry entry : this.entryBySrc) {
+                final Collection<Range> toModify = toBeEaten.stream()
+                        .filter(entry::overlapSrc)
+                        .collect(toSet());
+
+                final Collection<Range> replacements = toModify.stream()
+                        .<Range>mapMulti((r, acc) -> {
+                            final Range intersect = r.intersect(entry.src);
+                            eaten.add(new Range(entry.eval(intersect.start), entry.eval(intersect.end)));
+                            r.bisect(intersect).forEach(acc);
+                        })
+                        .collect(toSet());
+
+                toBeEaten.removeAll(toModify);
+                toBeEaten.addAll(replacements);
+            }
+
+            return Stream.concat(eaten.stream(), toBeEaten.stream()).collect(toSet());
+        }
+
         static AMap from(final Type srcType, final Collection<Entry> entries) {
             return new AMap(srcType, entries.stream().sorted(Comparator.comparing(Entry::src)).toList()
             );
@@ -180,6 +250,10 @@ public class Day05 {
             return this.dest.start + test - this.src.start;
         }
 
+        boolean overlapSrc(final Range range) {
+            return this.src.overlaps(range);
+        }
+
         static Entry from(final long dest, final long src, final long length) {
             return new Entry(new Range(dest, dest + length), new Range(src, src + length), length);
         }
@@ -193,6 +267,33 @@ public class Day05 {
 
         boolean isValid(final long test) {
             return test >= this.start && test < this.end;
+        }
+
+        boolean overlaps(final Range range) {
+            return this.start < range.end && this.end > range.start;
+        }
+
+        boolean contains(final Range range) {
+            return this.start < range.start && this.end > range.end;
+        }
+
+        Range intersect(final Range range) {
+            return new Range(Math.max(this.start, range.start), Math.min(this.end, range.end));
+        }
+
+        Collection<Range> bisect(final Range range) {
+            if (equals(range)) {
+                return Collections.emptySet();
+            }
+
+            if (contains(range)) {
+                return Set.of(
+                        new Range(this.start, range.start),
+                        new Range(range.end, this.end)
+                             );
+            }
+
+            return Set.of(range.intersect(this));
         }
 
         @Override
